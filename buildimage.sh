409e14b675f31b5fa31446bc5ed6501ab55dc67e
Lennart Kramer <lennart.kramer@stud.uni-goettingen.de>
2020-07-10 18:14:06 +0200

    Try committing to dockerhub

--- buildimage.sh
+++ buildimage.sh
@@ -3,21 +3,14 @@
 # arg2: maxima version
 # arg3: stack or moodle version: "stack-XXX" or "moodlev.X"
 # arg4: LIB_PATH
-# arg5: REGISTRY IP
+# arg5: REGISTRY or dockerhub id
 # arg6: version of goemaxima
 #
 echo "starting to build image for:"
 echo "sbcl: $1"
 echo "maxima: $2"
 echo "stack: $3"
-# tag the image
-if [ -n "$6" ]; then
-	IMAGENAME="$5/goemaxima-$3:$6"
-else
-	IMAGENAME="$5/goemaxima-$3:dev"
-fi
-# check if the image already exists on the server
-docker pull "${IMAGENAME}"
+IMAGENAME="goemaxima:$3"
 # build it
 if [ "$3" = "2017121800" ]; then
 	docker build -t "${IMAGENAME}" --build-arg MAXIMA_VERSION="$2" --build-arg SBCL_VERSION="$1" --build-arg LIB_PATH="$4" --build-arg "MAX_LIB_PATH=/opt/maxima/assets/maximalocal.mac" . || exit 1
@@ -25,6 +18,10 @@ else
 	docker build -t "${IMAGENAME}" --build-arg MAXIMA_VERSION="$2" --build-arg SBCL_VERSION="$1" --build-arg LIB_PATH="$4" . || exit 1
 fi
 echo "${IMAGENAME} wurde erfolgreich gebaut."
-# push it
-docker push "${IMAGENAME}"
-
+# push the image
+docker tag "$IMAGENAME" "$5/$IMAGENAME-dev"
+docker push "$5/$IMAGENAME-dev"
+if [ -n "$6" ]; then
+	docker tag "$IMAGENAME" "$5/$IMAGENAME-$6"
+	docker push "$5/$IMAGENAME-$6"
+fi
