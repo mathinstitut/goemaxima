aed5a0cf9b8d51103c68476b06ba10916433e68c
Lennart Kramer <lennart.kramer@stud.uni-goettingen.de>
2021-05-05 19:04:59 +0200

    fix buildscript again

--- buildweb.sh
+++ buildweb.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # This script compiles the web server application.
-
+set -e
 export GOBIN="$(realpath bin)"
-cd ./src/web && go install web
+cd ./src/web
+go mod download github.com/prometheus/client_golang
+go install web
 
