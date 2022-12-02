2c7da42f6404913cbcd59d57eaa3074638f711d5
Lennart Kramer <lennart.kramer@stud.uni-goettingen.de>
2022-12-02 15:03:18 +0100

    use CGO_ENABLED=0 for static build
    since the server executable is built outside of the docker image

--- buildweb.sh
+++ buildweb.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This script compiles the web server application.
 set -e
-export GOBIN="$(realpath bin)"
+export GOBIN="$(realpath bin)" CGO_ENABLED=0
 cd ./src/web
 go mod download all
 go install web
